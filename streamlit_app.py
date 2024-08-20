b18418beccc9c4c631480c8c33dc14515acdbca0
Sarowar / <113256659+sarowarahmed@users.noreply.github.com>
2024-08-20 22:38:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,16 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LinearRegression
+from sklaern.metrics import mean_squared_error, r2_score
+import matplotlib.pyplot as plt
 
-st.title('🎈 App Name')
+st.title('🔖 Advertising Sales Predictor')
 
-st.write('Hello world!')
+st.info('An app powered by a Machine Learning model, to forecast sales based on TV, Newspaper, and Online Advertising')
+
+#st.write('Hello world!')
